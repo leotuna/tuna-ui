51c459be38cd838e2ec2ec121fc1477600454b90
Leonardo Tuna <tunaleo@gmail.com>
2020-10-07 07:52:23 -0300

    button disabled style

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const ButtonStyled = styled.button`
-  cursor: pointer;
+  cursor: ${props => props.disabled ? 'not-allowed' : 'pointer'};
   border: ${(props) => (props.noBorder ? '1px solid transparent' : '1px solid #fd0')} ;
   border-radius: 0px;
   font-weight: 700;
@@ -10,4 +10,5 @@ export const ButtonStyled = styled.button`
   padding: 10px 0;
   color: black;
   background-color: ${(props) => (props.transparent ? 'transparent' : '#fd0')};
+  opacity: ${(props) => props.disabled ? 0.5 : 1};
 `;
